da7380813bfc7dd02f35c56cf2f8430c644b0875
luiz-hssd <lhenrique_diniz@hotmail.com>
2020-02-01 21:46:52 -0300

    Add project files.

---
+++ WebApplication10/Migrations/20200129002535_dev.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace WebApplication10.Migrations
+{
+    public partial class dev : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Weathers",
+                columns: table => new
+                {
+                    TemperatureC = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Summary = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weathers", x => x.TemperatureC);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Weathers");
+        }
+    }
+}
---
+++ WebApplication10/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication10
+{
+
+        public class MyContext : DbContext
+        {
+        public MyContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<WeatherForecast> Weathers { get; set; }
+
+            #region Required
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<WeatherForecast>()
+                    .Property(b => b.TemperatureC)
+                    .IsRequired();
+            }
+            #endregion
+        }
+
+}
